dc024abcc143c75a21330ef9df33a202c4d93409
duq <duque@intre.it>
2018-10-16 00:01:45 +0200

    Impression model created

---
+++ src/main/java/models/Impression.java
@@ -0,0 +1,41 @@
+package models;
+
+public class Impression {
+
+    private long deviceId;
+    private double lat;
+    private double lng;
+    private long timestamp;
+
+    public long getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(long deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
