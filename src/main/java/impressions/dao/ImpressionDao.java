9eaf5982810a7978bfa27a0b7fc3ccc0bd375a1d
duq <duque@intre.it>
2018-10-16 21:47:58 +0200

    Working on data access layer

---
+++ src/main/java/impressions/dao/ImpressionDao.java
@@ -0,0 +1,9 @@
+package impressions.dao;
+
+import impressions.model.Impression;
+
+import java.util.List;
+
+public interface ImpressionDao {
+    public List<Impression> getAllByDevice(long deviceId);
+}
---
+++ src/main/java/impressions/dao/ImpressionDaoCsv.java
@@ -0,0 +1,13 @@
+package impressions.dao;
+
+import impressions.model.Impression;
+
+import java.util.List;
+
+public class ImpressionDaoCsv implements ImpressionDao {
+
+    @Override
+    public List<Impression> getAllByDevice(long deviceId) {
+        return null;
+    }
+}
