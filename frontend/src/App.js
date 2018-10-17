7af243bb644fe97d5e7d8b13e6812b1a2ffcb007
duq <duque@intre.it>
2018-10-17 23:59:20 +0200

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    state = {};
+
+    componentDidMount() {
+        setInterval(this.getData, 3500);
+    }
+
+    getData = () => {
+        fetch('/api/impressions/for-each-hour')
+            .then(response => response.text())
+            .then(message => {
+                this.setState({eachHourData: message});
+            });
+        fetch('/api/impressions/from-each-device')
+            .then(response => response.text())
+            .then(message => {
+                this.setState({eachDeviceData: message});
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+                    <h1 className="App-title">{this.state.eachHourData}</h1>
+                    <h1 className="App-title">{this.state.eachDeviceData}</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
