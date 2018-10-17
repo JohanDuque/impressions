727e4b5247b37e7f092c3168bb53d4d6e197d0c6
duq <duque@intre.it>
2018-10-18 00:16:47 +0200

    both APIs test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,8 +28,9 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     {/*<img src={logo} className="App-logo" alt="logo"/>*/}
-                    <h1 className="App-title">{this.state.eachHourData}</h1>
-                    <h1 className="App-title">{this.state.eachDeviceData}</h1>
+                    <p className="App-title">{this.state.eachHourData}</p>
+                    <h1>TEST</h1>
+                    <p className="App-title">{this.state.eachDeviceData}</p>
                 </header>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
