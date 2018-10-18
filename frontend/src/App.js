af49f137c93f48f1b243354551695c643e3da04e
duq <duque@intre.it>
2018-10-18 23:06:52 +0200

    react-vis introduction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,73 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, HorizontalGridLines,
+    VerticalGridLines,
+    XAxis,
+    YAxis,
+    LineMarkSeries} from 'react-vis';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
 
-    state = {};
+        this.state = {
+            width: 1200,
+            height: 800,
+        };
+    }
+
+
 
     componentDidMount() {
-        setInterval(this.getData, 3500);
+        this.getData();
+        //setInterval(this.getData, 6500);
     }
 
     getData = () => {
         fetch('/api/impressions/for-each-hour')
-            .then(response => response.text())
-            .then(message => {
-                this.setState({eachHourData: message});
+            .then(response => response.json())
+            .then(json => {
+                this.setState({eachHourData: this.prepareChartData(json)});
             });
         fetch('/api/impressions/from-each-device')
-            .then(response => response.text())
-            .then(message => {
-                this.setState({eachDeviceData: message});
+            .then(response => response.json())
+            .then(json => {
+                this.setState({eachDeviceData: this.prepareChartData(json)});
             });
     };
 
+    prepareChartData(json) {
+        let data = [];
+        for (const keyName in json) {
+            let value = json[keyName];
+            data.push({x: parseInt(keyName), y: value});
+        }
+        return data;
+    };
+
     render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    {/*<img src={logo} className="App-logo" alt="logo"/>*/}
-                    <p className="App-title">{this.state.eachHourData}</p>
-                    <h1>TEST</h1>
-                    <p className="App-title">{this.state.eachDeviceData}</p>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
+
+        const { eachHourData, eachDeviceData } = this.state;
+
+        const data = eachHourData;
+
+        if (!data) {
+            return ( <div>
+                <p>Loading ...</p>
+                    <p>{data}</p>
+                </div>
+            );
+        }else {
+            return (
+
+                <XYPlot width={this.state.width} height={this.state.height}><XAxis/><YAxis/>
+                    <HorizontalGridLines/>
+                    <VerticalGridLines/>
+                    <LineMarkSeries data={data}/>
+                </XYPlot>
+            );
+        }
     }
 }
 
